344835f3a36eee46a598dd271d5ce05a9471b193
Thomas J. Leeper <thosjleeper@gmail.com>
2016-11-03 14:51:24 +0000

    support svyglm class (#1)

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# CHANGES TO prediction 0.1.2
+
+* Added support for "svglm" class from the **survey** package. (#1)
+
 # CHANGES TO prediction 0.1.0
 
 * Initial package released.
--- R/prediction_methods.R
+++ R/prediction_methods.R
@@ -55,6 +55,34 @@ prediction.glm <- function(model, data = find_data(model, parent.frame()), type 
 
 #' @rdname prediction
 #' @export
+prediction.svyglm <- function(model, data = find_data(model, parent.frame()), type = c("response", "link"), ...) {
+    # setup data
+    data <- data
+    
+    type <- match.arg(type)
+    
+    # reduce memory profile
+    model[["model"]] <- NULL
+    attr(model[["terms"]], ".Environment") <- NULL
+    
+    # extract predicted value at input value (value can only be 1 number)
+    pred <- predict(model, newdata = data, type = type, se.fit = TRUE, ...)
+    pred <- list(fitted = unclass(pred), 
+                 se.fitted = sqrt(unname(attributes(pred)[["var"]])))
+    attributes(pred[["fitted"]]) <- NULL
+    class(pred[["fitted"]]) <- c("fit", "numeric")
+    class(pred[["se.fitted"]]) <- c("se.fit", "numeric")
+    
+    # obs-x-2 data.frame of predictions
+    structure(pred, 
+              class = c("prediction", "data.frame"), 
+              row.names = seq_len(length(pred[["fit"]])),
+              model.class = class(model),
+              type = type)
+}
+
+#' @rdname prediction
+#' @export
 prediction.loess <- function(model, data = find_data(model, parent.frame()), type = "response", ...) {
     # setup data
     data <- data
