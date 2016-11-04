fa5b7d89b2e99d61e16773fb2f70f7ef8f5c171d
carlganz <carlganz@gmail.com>
2016-11-04 18:03:15 +0000

    make find_data generic and add method for svyglm

--- R/find_data.R
+++ R/find_data.R
@@ -10,11 +10,35 @@
 #' 
 #' @seealso \code{\link{prediction}}
 #' @export
-find_data <- function(model, env = parent.frame()) {
-    if (!is.null(model[["call"]][["data"]])) {
-        data <- eval(model[["call"]][["data"]], env) 
-    } else { 
-        data <- get_all_vars(model[["terms"]], data = model[["model"]])
-    }
-    data
+find_data <- function(model, env) {
+  UseMethod("find_data")
+  
+}
+
+#' @rdname find_data
+#' @export
+find_data.default <- function(model, env = parent.frame()) {
+  if (!is.null(model[["call"]][["data"]])) {
+    data <- eval(model[["call"]][["data"]], env) 
+  } else { 
+    data <- get_all_vars(model[["terms"]], data = model[["model"]])
+  }
+  data
+}
+
+#' @rdname find_data
+#' @export
+find_data.lm <- find_data.default
+
+#' @rdname find_data
+#' @export
+find_data.glm <- find_data.default
+
+#' @rdname find_data
+#' @export
+find_data.svyglm <- function(model, env = parent.frame()) {
+
+  data <- model$data
+
+  data
 }
