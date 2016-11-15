0e86f66b5fd5110281fb4155b97fabed455e62e0
Thomas J. Leeper <thosjleeper@gmail.com>
2016-11-15 16:08:20 +0000

    code formatting

--- R/find_data.R
+++ R/find_data.R
@@ -11,19 +11,18 @@
 #' @seealso \code{\link{prediction}}
 #' @export
 find_data <- function(model, env = parent.frame()) {
-  UseMethod("find_data")
-  
+    UseMethod("find_data")
 }
 
 #' @rdname find_data
 #' @export
 find_data.default <- function(model, env = parent.frame()) {
-  if (!is.null(model[["call"]][["data"]])) {
-    data <- eval(model[["call"]][["data"]], env) 
-  } else { 
-    data <- get_all_vars(model[["terms"]], data = model[["model"]])
-  }
-  data
+    if (!is.null(model[["call"]][["data"]])) {
+        data <- eval(model[["call"]][["data"]], env) 
+    } else { 
+        data <- get_all_vars(model[["terms"]], data = model[["model"]])
+    }
+    data
 }
 
 #' @rdname find_data
@@ -37,8 +36,6 @@ find_data.glm <- find_data.default
 #' @rdname find_data
 #' @export
 find_data.svyglm <- function(model, env = parent.frame()) {
-
-  data <- model$data
-
-  data
+    data <- model[["data"]]
+    data
 }
