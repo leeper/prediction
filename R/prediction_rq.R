ede5e38042e3672f19c8015bbc90b63d1a84bb4b
Thomas J. Leeper <thosjleeper@gmail.com>
2017-02-09 11:14:16 +0000

    add prediction.rq() method (#1)

---
+++ R/prediction_rq.R
@@ -0,0 +1,27 @@
+#' @rdname prediction
+#' @export
+prediction.rq <- 
+function(model, 
+         data = find_data(model, parent.frame()), 
+         type = NULL, 
+         ...) {
+    
+    # extract predicted value at input value
+    if (missing(data)) {
+        pred <- data.frame(fitted = predict(model, ...), se.fitted = NA_real_)
+    } else {
+        data <- data
+        pred <- data.frame(fitted = predict(model, newdata = data, ...), 
+                           se.fitted = NA_real_)
+    }
+    names(pred) <- c("fitted", "se.fitted")
+    class(pred[["fitted"]]) <- c("fit", "numeric")
+    class(pred[["se.fitted"]]) <- c("se.fit", "numeric")
+    
+    # obs-x-(ncol(data)+2) data.frame of predictions
+    structure(cbind(data, pred), 
+              class = c("prediction", "data.frame"), 
+              row.names = seq_len(length(pred[["fitted"]])),
+              model.class = class(model),
+              type = type)
+}
