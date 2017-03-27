32db4862968ea9eb935324d96c4e17a4e138d647
Thomas J. Leeper <thosjleeper@gmail.com>
2017-03-27 10:07:46 +0100

    find_data works with subset (closes #15)

--- tests/testthat/tests-find_data.R
+++ tests/testthat/tests-find_data.R
@@ -15,21 +15,17 @@ test_that("Test find_data.default()", {
     expect_true(nrow(find_data(m2)) == nrow(mtcars2[-c(1:3), ]), label = "find_data.default(data, na.action) works")
     
     m3 <- lm(mpg ~ cyl, data = mtcars2, subset = am == 1)
-    expect_true(nrow(find_data(m3)) == nrow(mtcars2[mtcars2$am == 1, ]), label = "find_data.default(data, subset, na.action) works")
+    expect_true(nrow(find_data(m3)) == nrow(na.omit(mtcars2[mtcars2$am == 1, ])), label = "find_data.default(data, subset, na.action) works")
     
     expect_error(find_data(StructTS(log10(UKgas), type = "BSM")), label = "find_data.default([no formula]) errors")
 })
 
-test_that("Test find_data.llm()", {
+test_that("Test find_data.lm()", {
     expect_true(inherits(find_data(lm(mpg ~ cyl, data = mtcars)), "data.frame"), label = "find_data.lm() works")
 })
 
 test_that("Test find_data.glm()", {
     expect_true(inherits(find_data(glm(mpg ~ cyl, data = mtcars)), "data.frame"), label = "find_data.glm() works")
-})
-
-test_that("Test find_data.formula()", {
-    expect_true(inherits(find_data(mpg ~ cyl, data = mtcars), "data.frame"), label = "find_data.formula() works")
 })
 
 test_that("Test find_data.data.frame()", {
