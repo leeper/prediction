fb44b2ff7798fa0e417d7436250e9b7416bb9b75
Thomas J. Leeper <thosjleeper@gmail.com>
2017-03-27 10:22:20 +0100

    reorganize docs

--- R/prediction-package.R
+++
@@ -1,9 +0,0 @@
-#' @name prediction-package
-#' @title Tidy, Type-safe \code{prediction()} Methods
-#' @aliases prediction-package
-#' @docType package
-#' @description This package provides tidy utilities for dealing with model predictions. The main function is \code{\link{prediction}}, a type-safe wrapper around \code{\link[stats]{predict}} that always returns a data frame. Other functions, such as \code{\link{find_data}}, \code{\link{mean_or_mode}}, and so forth provide some convenience tools for generating predictions.
-#' @author Thomas J. Leeper
-#' @keywords package
-#' @seealso \code{\link{prediction}}
-NULL
