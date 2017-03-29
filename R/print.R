6f1e53545d94dd443e9f98321e4b5b3f20e4602b
Thomas J. Leeper <thosjleeper@gmail.com>
2017-03-29 15:52:57 +0100

    improve factor outcome printing

--- R/print.R
+++ R/print.R
@@ -2,29 +2,32 @@
 #' @export
 print.prediction <- function(x, digits = 4, ...) {
     f <- x[["fitted"]]
+    fc <- x[["fitted.class"]]
     if (is.null(attributes(x)[["at"]])) {
-        if (is.numeric(f)) {
+        if (!"fitted.class" %in% names(x)) {
+            # numeric outcome
             m <- sprintf(paste0("%0.", digits, "f"), mean(f, na.rm = TRUE))
             message(paste0("Average prediction for ", length(f), " ", ngettext(length(f), "observation", "observations"), ": ", m))
         } else {
-            m <- sort(table(x[["fitted"]]), decreasing = TRUE)[1L]
-            cat(paste0("Modal prediction:  for ", m, " of ", length(f), " ", 
-                ngettext(length(f), "observation", "observations"),
-                " with total ", nlevels(f), " ", ngettext(nlevels(f), "level", "levels"), ": ", shQuote(names(m)) ))
+            # factor outcome
+            m <- sort(table(x[["fitted.class"]]), decreasing = TRUE)[1L]
+            message(paste0("Modal prediction (of ", nlevels(factor(fc)), " ", ngettext(nlevels(f), "level", "levels"), 
+                    ") for ", length(fc), " ", ngettext(length(fc), "observation", "observations"), ": ", shQuote(names(m))))
         }
     } else {
         xby <- x[ , attributes(x)[["at"]], drop = FALSE]
-        if (is.numeric(f)) {
+        if (!"fitted.class" %in% names(x)) {
+            # numeric outcome
             out <- aggregate(x[["fitted"]], xby, FUN = mean, na.rm = TRUE)
             message(paste0("Average ", ngettext(nrow(out), "prediction", "predictions"), 
                     " for ", length(f)/nrow(out), " ", 
                     ngettext(nrow(out), "observation", "observations"), ":"))
         } else {
-            out <- aggregate(x[["fitted"]], xby, FUN = function(set) sort(table(set), decreasing = TRUE)[1L])
+            # factor outcome
+            out <- aggregate(x[["fitted.class"]], xby, FUN = function(set) names(sort(table(set), decreasing = TRUE))[1L])
             message(paste0("Modal ", ngettext(nrow(out), "prediction", "predictions"), 
-                    " for ", m, " of ", length(f), " ", 
-                    ngettext(length(f), "observation", "observations"),
-                    " with total ", nlevels(f), " ", ngettext(nlevels(f), "level", "levels"), ":"))
+                    " (of ", nlevels(factor(fc)), " ", ngettext(nlevels(factor(fc)), "level", "levels"), 
+                    ") for ", length(fc), " ", ngettext(length(fc), "observation", "observations"), ": "))
         }
         names(out)[names(out) != "x"] <- paste0("at(", names(out)[names(out) != "x"], ")")
         names(out)[names(out) == "x"] <- "value"
